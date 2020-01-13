1381e2e5c0bb6c762c15f0db9b86c61a54adf06e
Michael Scharl <michael.scharl@massiveart.com>
2020-01-13 14:53:08 +0100

    Refactored the styles

--- src/components/AlphabeticScrollBar.js
+++ src/components/AlphabeticScrollBar.js
@@ -112,16 +112,15 @@ const styles = {
         marginTop: 20,
         width: 30,
         position: 'absolute',
-        right: 12,
+        right: 0,
         top: 0,
-        bottom: '2%',
+        bottom: 0,
         flexDirection: 'column',
         justifyContent: 'space-between',
         alignItems: 'center'
     },
     letter: {
         alignSelf: 'center',
-        color: '#8e898e',
     }
 };
 
